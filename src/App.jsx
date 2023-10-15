cb68400456fad63eda877e445e5c1890395eff3b
f0b <francisefobi@gmail.com>
2023-10-15 11:47:48 +0100

    desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion'
 import './App.css'
 
 import pattern_divider_mobile from '/assets/images/pattern-divider-mobile.svg'
+import pattern_divider_desktop from '/assets/images/pattern-divider-desktop.svg'
 import icon_dice from '/assets/images/icon-dice.svg'
 
 function App() {
@@ -39,23 +40,39 @@ function App() {
   return (
     <>
       <div className=' bg-[hsl(218,23%,16%)] h-screen text-white px-3 flex items-center text-[28px]'>
-        <div className=' bg-[hsl(217,19%,24%)] w-full h-1/2 relative flex flex-col items-center justify-between px-4 pt-4 pb-16 rounded-lg'>
-          <p className=' uppercase text-sm pt-3 tracking-[0.2em] text-[hsl(150,100%,66%)]'>{`Advice #${advices.id}`}</p>
-          <p className=' text-center font-manrope py-2 text-[hsl(193,38%,86%)]'>{`"${advices.advice} ${error}"`}</p>
+        <div className=' bg-[hsl(217,19%,24%)] w-full h-1/2 relative flex flex-col items-center justify-between px-4 pt-4 pb-16 rounded-lg lg:max-w-lg lg:mx-auto lg:px-5'>
+          {error ? (
+            <p className=' text-center font-manrope py-2 text-[hsl(193,38%,86%)]'>{`"${error}"`}</p>
+            ):(
+            <>
+              <p className=' uppercase text-sm font-manrope pt-3 tracking-[0.2em] text-[hsl(150,100%,66%)]'>{`Advice #${advices.id}`}</p>
+              <p className=' text-center font-manrope py-2 text-[hsl(193,38%,86%)]'>{`"${advices.advice}"`}</p>
+            </>
+          )}
 
-          <img src={pattern_divider_mobile} alt="divider-mobile" className=' w-full' />
+          <img src={pattern_divider_mobile} alt="divider-mobile" className=' w-full lg:hidden'/>
+          <img src={pattern_divider_desktop} alt="divider-desktop" className=' hidden lg:block'/>
 
-          <motion.button 
-            animate = {{scale: [1, 1.1, 1]}}
-            transition={{
-              duration: 0.5,
-              ease: "easeInOut",
-              repeat: Infinity,
-            }}
-            onClick={generateAdviceBtn} 
-            className=' absolute -bottom-8 w-16 h-16 rounded-full bg-[hsl(150,100%,66%)] flex justify-center items-center'>
-            <img src={icon_dice} alt="icon-dice" />
-          </motion.button>
+          <div className=' absolute -bottom-8 flex justify-center items-center'>
+            <motion.button 
+              className=' w-20 h-20 rounded-full bg-[hsl(150,100%,66%)]/40'
+              animate = {{scale: [1, 1.1, 1]}}
+              transition={{
+                duration: 0.5,
+                ease: "easeInOut",
+                repeat: Infinity,
+              }}
+            />
+
+            <motion.button 
+              onClick={generateAdviceBtn} 
+              className=' absolute w-16 h-16 rounded-full bg-[hsl(150,100%,66%)] flex justify-center items-center'
+            >
+              <img src={icon_dice} alt="icon-dice" />
+            </motion.button>
+          </div>
+
+          
         </div>
       </div>
 
